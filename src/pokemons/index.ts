0abe58c161847e2156384a7fa55208badd54b8e2
Alejandro-Arroyave <achoarro@hotmail.com>
2024-03-18 18:21:46 -0500

    Sección 5: Generación dinámica - SSR

---
+++ src/pokemons/index.ts
@@ -0,0 +1,7 @@
+// Components
+export { PokemonGrid } from "./components/PokemonGrid";
+
+// Types
+export type { SimplePokemon } from "./interfaces/simple-pokemon";
+export type { PokemonsResponse } from "./interfaces/pokemons-response";
+export type { Pokemon } from "./interfaces/pokemon";
