581d8595806a84f65aec4488e76fa9b9491c917c
Alejandro-Arroyave <achoarro@hotmail.com>
2024-03-18 15:10:08 -0500

    Sección 4: Server Side + Client Side Rendering

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,14 @@
+import { CartCounter } from "@/app/shopping-cart";
+
+export const metadata = {
+  title: "Counter Page",
+  description: "Counter Page Description",
+};
+
+export default function CounterPage() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <CartCounter value={20} />
+    </div>
+  );
+}
